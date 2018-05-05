e5e5dbd3cb30d648dfc427954a66f4ceba07aa3b
lonelion <lonelion@users.noreply.github.com>
2018-05-05 14:49:19 -0700

    [#28] Video title should be get based on the context/selected element [#29] The user app should define their custom getVideoTitle function in order to be tracked by the Analytics framework
    Besides, a NetflixUtils js library was created.

--- js/imdb.js
+++ js/imdb.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 	const imdbUrlTemplate = `http://www.imdb.com/find?ref_=nv_sr_fn&q=VIDEO_TITLE&s=all`;
-	let previousSelectedTitleElement = 'undefined';
 
 	let imdbImage = $("<span/>");
 	imdbImage.css({
@@ -8,7 +7,6 @@ $(document).ready(function () {
 		"height": "auto"
 	});
 	imdbImage.append("<img id=\"imdb_img\" src=\"equo/IMDB_logo_2016.png\" style=\"max-width:60px;height:auto;\" alt=\"IMDB\" />")
-	//imdbImage.append("<img id=\"imdb_img\" src=\"file://Users/seba/myRepository/equo-product/netflix-desktop-app/bin/IMDB_logo_2016.png\" alt=\"IMDB\" />")
 
 	let imdbLink = $("<a/>", {
 		role: "link",
@@ -25,33 +23,14 @@ $(document).ready(function () {
 	});
 	imdbDiv.append(divWrapper).append(imdbLink);
 
-	const getVideoTitle = function () {
-		let outerElement = $('.title.has-jawbone-nav-transition');
-		let currentTitle = outerElement.first();
-		if (currentTitle[0] == previousSelectedTitleElement) {
-			currentTitle = outerElement.last();
-		}
-		previousSelectedTitleElement = currentTitle[0];
-		let imgTitle = $(currentTitle).children('img.logo');
-		let altAttr = imgTitle.attr('alt');
-		let videoTitle;
-		if (typeof altAttr !== 'undefined') {
-			videoTitle = altAttr;
-		} else {
-			let divTitle = $(currentTitle).children('div.text');
-			videoTitle = divTitle.text();
-		}
-		return videoTitle;
-	};
-
 	const getImdbUrl = function (videoTitle) {
 		let imdbVideoTitle = videoTitle.replace(/ /g, '+');
 		let imdbUrl = imdbUrlTemplate.replace('VIDEO_TITLE', imdbVideoTitle);
 		return imdbUrl;
 	};
 
-	const refreshImdbPage = function () {
-		let videoTitle = getVideoTitle();
+	const refreshImdbPage = function (child) {
+		let videoTitle = NetflixUtils.getVideoTitle(child);
 		let imdbUrl = getImdbUrl(videoTitle);
 		equo.updateBrowser({
 			url: imdbUrl,
@@ -59,17 +38,28 @@ $(document).ready(function () {
 		});
 	};
 
+	const insertImdbDivInBillboardLinks = function () {
+		let optionalBillboardLinks = $(document).find('.billboard-links');
+		if (optionalBillboardLinks.length) {
+			let billboardLinks = $(optionalBillboardLinks[0]);
+			imdbDiv.insertAfter(billboardLinks);
+			refreshImdbPage(billboardLinks);
+		}
+	};
+
+	insertImdbDivInBillboardLinks();
+
 	equo.onNativeDomChanged((addedNode) => {
 		let optionalOverview = addedNode.find('#pane-Overview div.overview');
 		if (optionalOverview.length) {
 			let overviewElement = $(optionalOverview[0]);
 			imdbDiv.insertAfter(overviewElement);
-			refreshImdbPage();
+			refreshImdbPage(imdbDiv);
 		}
 	});
 
 	imdbDiv.click(function () {
-		let videoTitle = getVideoTitle();
+		let videoTitle = NetflixUtils.getVideoTitle(imdbDiv);
 		let imdbUrl = getImdbUrl(videoTitle);
 		equo.openBrowser({
 			url: imdbUrl,
@@ -77,5 +67,4 @@ $(document).ready(function () {
 			position: 'bottom'
 		});
 	});
-
 });
